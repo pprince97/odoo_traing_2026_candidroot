84242f422ef0a4cfb973a676a5fb5f3079f459a3
Smit <mungarasmit@gmail.com>
2026-03-23 18:03:09 +0530

    Adding website module

---
+++ website_custom/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': "Website",
+
+    'summary': "This module is for product management.",
+
+    'description': "This module is for product management.",
+
+    'author': "CandidRoot Solutions PVT LTD",
+    'license': 'LGPL-3',
+    'category': 'Uncategorized',
+    'version': '19.0.0.1',
+
+    'depends': ['base','website', 'library_management_smit'],
+
+    'data': [
+        'views/custom_home.xml',
+    ],
+
+
+
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
