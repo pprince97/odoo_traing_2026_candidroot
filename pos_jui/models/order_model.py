1b8275f7a4797b063235cf803d064213b3f2fc84
erp <pjp006np@gmail.com>
2026-04-07 19:12:09 +0530

    07-04-26 jui

---
+++ pos_jui/models/order_model.py
@@ -0,0 +1,17 @@
+from odoo import models,fields,api,_
+from datetime import datetime
+
+class OrderModel(models.Model):
+    _inherit = "pos.order"
+
+    start_date = fields.Datetime("Start Date")
+    end_date = fields.Datetime("End Date")
+    differ = fields.Char("Difference(date)",compute="_compute_difference")
+
+    def _compute_difference(self):
+        for order in self:
+            if order.start_date and order.end_date:
+                diff = order.end_date - order.start_date
+                order.differ = str(diff)
+            else:
+                order.differ = "N/A"
--- pos_jui/static/src/js/floor_screen.js
+++ pos_jui/static/src/js/floor_screen.js
@@ -1,32 +1,84 @@
+import { FloorScreen } from "@pos_restaurant/app/screens/floor_screen/floor_screen";
 import { patch } from "@web/core/utils/patch";
-import { FloorScreen } from "@point_of_sale/app/screens/floor_screen/floor_screen";
+import { onWillUnmount, onMounted } from "@odoo/owl";
+import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product_screen";
+import { PosStore } from "@point_of_sale/app/services/pos_store";
+import { PosOrder } from "@point_of_sale/app/models/pos_order";
+import OrderPaymentValidation from "@point_of_sale/app/utils/order_payment_validation";
+import { serializeDateTime } from "@web/core/l10n/dates";
+const { DateTime } = luxon;
 
-patch(FloorScreen.prototype,{
+
+
+patch(PosOrder.prototype, {
+    setup(_defaultObj, options) {
+        super.setup(...arguments);
+        this.start_date = this.start_date || null;
+        this.end_date = this.end_date || null;
+    },
+    export_as_JSON() {
+        const json = super.export_as_JSON(...arguments);
+        if (json) {
+            json.start_date = this.start_date;
+            json.end_date = this.end_date;
+        }
+        return json;
+    },
+    init_from_JSON(json) {
+        super.init_from_JSON(...arguments);
+        this.start_date = json.start_date;
+        this.end_date = json.end_date;
+    },
+});
+
+patch(ProductScreen.prototype, {
+    async addProductToOrder(product, options) {
+        const result =await super.addProductToOrder(...arguments);
+        const order = this.currentOrder;
+        if (!order.start_date && order.lines.length > 0) {
+            this.order.start_date = serializeDateTime(DateTime.now());
+        }
+        return result;
+    },
+});
+
+patch(OrderPaymentValidation.prototype, {
+    async validateOrder() {
+        if (!this.order.end_date) {
+            this.order.end_date = serializeDateTime(DateTime.now());
+        }
+        return await super.validateOrder(...arguments);
+    },
+});
+
+patch(FloorScreen.prototype, {
     setup() {
         super.setup();
-        this.counter = new Date();
-        this.counter.setHours(0, 0, 0, 0);
-        this.display = document.getElementById('counter_p');
-    },
+        this.state.tableTimers = {};
+        let interval;
 
-    startTimer() {
-        this.startTime = Date.now(); // Record the exact start moment
+        onMounted(()=>{
+            interval = setInterval(() => {
+                const floor = this.pos.models["restaurant.floor"].get(this.state.selectedFloorId);
 
-        this.timerInterval = setInterval(() => {
-            const elapsedMs = Date.now() - startTime;
-            this.display.innerText = formatTime(elapsedMs);
-        }, 1000); // Update every 1000ms (1 second)
-    },
+                floor.table_ids.forEach((table) => {
+                    let order = this.pos.models["pos.order"].filter(
+                        (o) => o.table_id?.id === table.id && !o.finalized
+                    );
+                    order = order[0]
+                    if (order && order.lines.length > 0) {
 
-    stopTimer() {
-        clearInterval(this.timerInterval); // Stop the counting
-        console.log("Final time:", this.display.innerText);
-    },
+                        const diff = Math.floor((new Date() - new Date(order.start_date)) / 1000);
+                        const mins = Math.floor(diff / 60).toString().padStart(2, '0');
+                        const secs = (diff % 60).toString().padStart(2, '0');
+                        this.state.tableTimers[table.id] = `${mins}:${secs}`;
+                    } else {
+                        this.state.tableTimers[table.id] = '00:00';
+                    }
+                });
+            }, 1000);
+        });
 
-    formatTime(ms) {
-        const totalSeconds = Math.floor(ms / 1000);
-        const minutes = Math.floor(totalSeconds / 60).toString().padStart(2, '0');
-        const seconds = (totalSeconds % 60).toString().padStart(2, '0');
-        return `${minutes}:${seconds}`;
-    },
-})
+        onWillUnmount(() => {if(interval){clearInterval(interval)}});
+    }
+});
