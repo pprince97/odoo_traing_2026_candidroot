7e070e99c8c389c34556b1cb4a2b239855b3a58f
Smit <mungarasmit@gmail.com>
2026-04-13 18:36:25 +0530

    Update In POS

--- pos_guest_info/models/pos_order.py
+++ pos_guest_info/models/pos_order.py
@@ -4,17 +4,3 @@ class PosOrder(models.Model):
     _inherit = "pos.order"
 
     guest_ids = fields.One2many('pos.guest', 'order_id', string="Guest")
-
-    @api.model_create_multi
-    def create(self, vals_list):
-        for vals in vals_list:
-            # Look for guest_ids in the values coming from JS
-            print(vals)
-            if 'guest_ids' in vals:
-                print("!!! CREATE CAUGHT GUEST_IDS !!!", vals['guest_ids'])
-        return super().create(vals_list)
-
-    def write(self, vals):
-        if 'guest_ids' in vals:
-            print("!!! WRITE CAUGHT GUEST_IDS !!!", vals['guest_ids'])
-        return super().write(vals)
